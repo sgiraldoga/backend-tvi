4408731f4e0d4684bb3b29de3ef847206037a078
Jose Miguel Pulgarin Agudelo <josemiguelp@solenium.co>
2025-12-05 23:39:44 -0500

    chore: se implementa lógica de servicio de destinos con operaciones CRUD completas y se elimina archivo de pruebas del controlador

--- src/destinies/destinies.service.ts
+++ src/destinies/destinies.service.ts
@@ -1,26 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateDestinyDto } from './dto/create-destiny.dto';
 import { UpdateDestinyDto } from './dto/update-destiny.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Destiny } from './entities/destiny.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class DestiniesService {
-  create(createDestinyDto: CreateDestinyDto) {
-    return 'This action adds a new destiny';
+  private readonly defaultSelect: (keyof Destiny)[] = [
+    'id',
+    'name',
+    'description',
+    'image',
+    'system',
+    'gravity',
+    'atmosphere',
+    'dayNightCycle',
+    'population',
+    'averageTemperature',
+    'distance',
+  ];
+
+  constructor(
+    @InjectRepository(Destiny)
+    private readonly destinyRepository: Repository<Destiny>,
+  ) {}
+
+  async create(createDestinyDto: CreateDestinyDto) {
+    const destiny = this.destinyRepository.create(createDestinyDto);
+    const savedDestiny = await this.destinyRepository.save(destiny);
+
+    return savedDestiny;
   }
 
-  findAll() {
-    return `This action returns all destinies`;
+  async findAll() {
+    return this.destinyRepository.find({
+      select: this.defaultSelect,
+      order: { name: 'DESC' },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} destiny`;
+  async findOne(id: number) {
+    const destiny = await this.destinyRepository.findOne({
+      select: this.defaultSelect,
+      where: { id },
+    });
+
+    if (!destiny) {
+      throw new NotFoundException(`Destino con ID ${id} no encontrado`);
+    }
+
+    return destiny;
   }
 
-  update(id: number, updateDestinyDto: UpdateDestinyDto) {
-    return `This action updates a #${id} destiny`;
+  async update(id: number, updateDestinyDto: UpdateDestinyDto) {
+    if (Object.keys(updateDestinyDto).length === 0) {
+      throw new BadRequestException('No hay campos para actualizar');
+    }
+
+    const destiny = await this.findOne(id);
+    Object.assign(destiny, updateDestinyDto);
+
+    return this.destinyRepository.save(destiny);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} destiny`;
+  async remove(id: number) {
+    const destiny = await this.findOne(id);
+
+    return this.destinyRepository.remove(destiny);
   }
 }
